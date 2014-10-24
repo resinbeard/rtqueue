f0c5ea6b97937d4229ae358ac2ee5e8ff6eed5a2
murray <murray.foster@accellion.com>
2014-10-24 09:59:48 -0700

    -Fixed: Moved executable code out of header file.

--- test_rtqueue.c
+++ test_rtqueue.c
@@ -15,6 +15,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 Copyright 2014 murray foster 
 mrafoster at gmail dawt com */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include "rtqueue.h"
 
